663e63e16c9a1e53670b2af6c9bc2b50b9a2b276
Jeff Sickel <jas@corpus-callosum.com>
2012-10-17 20:07:18 -0500

    update libmemlayer to current Plan 9 version

--- libmemlayer/lalloc.c
+++ libmemlayer/lalloc.c
@@ -5,7 +5,7 @@
 #include <memlayer.h>
 
 Memimage*
-memlalloc(Memscreen *s, Rectangle screenr, Refreshfn refreshfn, void *refreshptr, ulong val)
+memlalloc(Memscreen *s, Rectangle screenr, Refreshfn refreshfn, void *refreshptr, u32int val)
 {
 	Memlayer *l;
 	Memimage *n;
@@ -19,7 +19,7 @@ memlalloc(Memscreen *s, Rectangle screenr, Refreshfn refreshfn, void *refreshptr
 		paint->clipr = Rect(-0x3FFFFFF, -0x3FFFFFF, 0x3FFFFFF, 0x3FFFFFF);
 	}
 
-	n = allocmemimaged(screenr, s->image->chan, s->image->data, s->image->X);
+	n = allocmemimaged(screenr, s->image->chan, s->image->data);
 	if(n == nil)
 		return nil;
 	l = malloc(sizeof(Memlayer));
